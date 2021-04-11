e8ff87c217f78ce1d59cbcfb0bcb22491c7e6a9a
Paul Meyer <paulmeyerber@gmail.com>
2021-04-11 18:22:22 +0200

    FIXUP

--- lib/apollo.tsx
+++ lib/apollo.tsx
@@ -130,7 +130,7 @@ function createApolloClient(initialState = {}) {
 	return new ApolloClient({
 		ssrMode: typeof window === "undefined", // Disables forceFetch on the server (so queries are only run once)
 		link: new HttpLink({
-			uri:'http://localhost:9000/graphql', // Server URL (must be absolute)
+			uri:'https://agency-grapqhl-api.herokuapp.com/graphql', // Server URL (must be absolute)
 			credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
 			headers: { authorization: "TOKEN" },
 			fetch,
